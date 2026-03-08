33b2772d9d6fb264d7867eeeac1716c02ef0ee6b
akademija <akademija@MacBook-Pro-od-akademija.local>
2026-03-08 20:11:24 +0100

    Setup za projekt

---
+++ Sofascore2026/Sofascore2026/Sofascore2026App.swift
@@ -0,0 +1,17 @@
+//
+//  Sofascore2026App.swift
+//  Sofascore2026
+//
+//  Created by akademija on 08.03.2026..
+//
+
+import SwiftUI
+
+@main
+struct Sofascore2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
